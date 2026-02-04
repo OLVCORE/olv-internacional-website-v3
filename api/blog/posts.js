99fcf3e312b388cf84a49ec018a412a06ce4f178
Marcos Oliveira <olvsistemas@olvinternacional.com.br>
2026-02-04 11:26:18 -0300

    API posts: Cache-Control 30s para novos posts aparecerem no site apÃ³s ingestÃ£o

--- api/blog/posts.js
+++ api/blog/posts.js
@@ -20,6 +20,8 @@ module.exports = async (req, res) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Access-Control-Allow-Methods', 'GET, OPTIONS');
     res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+    // Evitar cache longo para novos posts aparecerem no site logo após ingestão
+    res.setHeader('Cache-Control', 'public, max-age=30, s-maxage=60');
 
     // Handle preflight
     if (req.method === 'OPTIONS') {
